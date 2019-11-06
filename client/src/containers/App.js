17792cde66e4bb815445b7caa744ab1a3abd7594
Jake Lewochko <jlewochko@gmail.com>
2019-11-06 04:46:39 -0500

    moved files to client folder and moved backend to root

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import './App.css';
-import NavBar from '../components/nav_bar/NavBar'
-import {Route, Redirect} from "react-router-dom";
-import WatchingVideoPage from "./WatchingVideoPage";
+import NavBar from '../components/nav_bar/NavBar';
+import { Route, Redirect } from 'react-router-dom';
+import WatchingVideoPage from './WatchingVideoPage';
 import VideoUpload from '../components/video_upload/VideoUpload';
-console.log('page refreshed')
 export default class App extends Component {
-
-  
-
   render() {
-
     return (
       <div className="App">
-      <NavBar />
-<Route exact path="/" render={() => <Redirect to='/watch/1' /> } />
-      
-      <Route path='/publish' exact render={(props => <VideoUpload />)} />
-      <Route path='/watch/:id' exact render={(props => <WatchingVideoPage  current={props.match.params.id} />)} />
+        <NavBar />
+        <Route exact path="/" render={() => <Redirect to="/watch/1" />} />
 
+        <Route path="/publish" exact render={props => <VideoUpload />} />
+        <Route
+          path="/watch/:id"
+          exact
+          render={props => (
+            <WatchingVideoPage current={props.match.params.id} />
+          )}
+        />
       </div>
     );
   }
--- routes/VideoRoutes.js
+++ routes/VideoRoutes.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const videoServices = require('../services/videoService');
-const videos = require('../videos')
+const videos = require('../videos');
 
 router
-  .route("/")
+  .route('/')
   .get((req, res) => {
-    console.log(req);
-    
     res.status(200).json(videoServices.getVideoSuggestions());
   })
   .post(function(req, res) {
@@ -23,25 +21,34 @@ router
       res
         .status(400)
         .json(
-          "Required Fields were not Included in request include name and comment"
+          'Required Fields were not Included in request include name and comment'
         );
     }
   });
 
 router
-  .route("/:videoId")
-  .get((req, res) => videoServices.videoIdFunc(req.params.videoId) ? res.status(200).json(videoServices.videoIdFunc(req.params.videoId)): res.status(404).send('Page Not Found'))
+  .route('/:videoId')
+  .get((req, res) =>
+    videoServices.videoIdFunc(req.params.videoId)
+      ? res.status(200).json(videoServices.videoIdFunc(req.params.videoId))
+      : res.status(404).send('Page Not Found')
+  )
   .put((req, res) => {
-    if (req.body.title && req.body.description && videoServices.videoIdFunc(req.params.videoId)) {
+    if (
+      req.body.title &&
+      req.body.description &&
+      videoServices.videoIdFunc(req.params.videoId)
+    ) {
       videoServices.videoIdFunc(req.params.videoId).title = req.body.title;
-      videoServices.videoIdFunc(req.params.videoId).description = req.body.description;
+      videoServices.videoIdFunc(req.params.videoId).description =
+        req.body.description;
       videoServices.videoIdFunc(req.params.videoId).updated = Date.now();
       res.status(200).json(videoServices.videoIdFunc(req.params.videoId));
     } else {
       res
         .status(400)
         .json(
-          "Required Fields were not Included in request include title and description or item does not exist"
+          'Required Fields were not Included in request include title and description or item does not exist'
         );
     }
   })
@@ -50,7 +57,7 @@ router
     res.status(200).json(videos);
   });
 
-router.route("/:videoId/comments").post(function(req, res) {
+router.route('/:videoId/comments').post(function(req, res) {
   if (req.body.name && req.body.comment) {
     let newComment = videoServices.commentObj(
       req.params.videoId,
@@ -58,40 +65,50 @@ router.route("/:videoId/comments").post(function(req, res) {
       req.body.comment
     );
     videoServices.videoIdFunc(req.params.videoId).comments.push(newComment);
-    res.status(200).json(videoServices.videoIdFunc(req.params.videoId).comments);
+    res
+      .status(200)
+      .json(videoServices.videoIdFunc(req.params.videoId).comments);
   } else {
     res
       .status(400)
       .json(
-        "Required Fields were not Included in request include name and comment"
+        'Required Fields were not Included in request include name and comment'
       );
   }
 });
 router
-  .route("/:videoId/comments/:commentId")
+  .route('/:videoId/comments/:commentId')
   .put((req, res) => {
     if (req.body.comment && videoServices.videoIdFunc(req.params.videoId)) {
-      videoServices.videoIdFunc(req.params.videoId).comments.find(
-        item => item.id === parseInt(req.params.commentId)
-      ).comment =
-        req.body.comment;
-        videoServices.videoIdFunc(req.params.videoId).comments.find(
-        item => item.id === parseInt(req.params.commentId)
-      ).updated = Date.now();
-      res.status(200).json(videoServices.videoIdFunc(req.params.videoId).comments);
+      videoServices
+        .videoIdFunc(req.params.videoId)
+        .comments.find(
+          item => item.id === parseInt(req.params.commentId)
+        ).comment = req.body.comment;
+      videoServices
+        .videoIdFunc(req.params.videoId)
+        .comments.find(
+          item => item.id === parseInt(req.params.commentId)
+        ).updated = Date.now();
+      res
+        .status(200)
+        .json(videoServices.videoIdFunc(req.params.videoId).comments);
     } else {
       res
         .status(400)
         .json(
-          "Required Fields were not Included in request include name and comment or item does not exist"
+          'Required Fields were not Included in request include name and comment or item does not exist'
         );
     }
   })
   .delete(function(req, res) {
-    videos[videos.indexOf(videoServices.videoIdFunc(req.params.videoId))].comments = videos[
+    videos[
+      videos.indexOf(videoServices.videoIdFunc(req.params.videoId))
+    ].comments = videos[
       videos.indexOf(videoServices.videoIdFunc(req.params.videoId))
     ].comments.filter(item => item.id !== parseInt(req.params.commentId));
-    res.status(200).json(videoServices.videoIdFunc(req.params.videoId).comments);
+    res
+      .status(200)
+      .json(videoServices.videoIdFunc(req.params.videoId).comments);
   });
 module.exports = router;
- 
